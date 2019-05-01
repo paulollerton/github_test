ced78eaf9e109e1ae0c92cc1ba5758321aa21dae
paulollerton <paul.ollerton@gmail.com>
2019-04-30 22:36:57 -0700

    file

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+apple = {item: "Apple", color: "green", price: ".5"}
+T800 = {item: "Terminator", color: "Arnold", price: "100"}
+NCC1701D = {item: "USS Enterprise", color: "gray", price: "1,000,000,000,000"}
+
+puts "#{apple[:item]} is #{apple[:color]}"
